01c57224023ceebc2909a8334ded0e4df5029d7a
Patrick O'Brien <patrick@commonbond.co>
2018-08-24 23:45:02 -0400

    Fixes run bug

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ use lattice::{Lattice, square};
 fn main() {
     let lat: square::Square = Lattice::new(4, 4);
 
+
     println!("{}", lat);
 
     let mut model: models::ising::Ising = models::Model::new(lat);
 
-    for _ in 0..10 {
-        model.flip_spin();
-        println!("{}", model.get_energy());
-    }
+//    for _ in 0..10 {
+//        model.flip_spin();
+//        println!("{}", model.get_energy());
+//    }
 }
